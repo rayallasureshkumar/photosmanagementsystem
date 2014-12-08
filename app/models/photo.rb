64d11a8f107da7db5bd940636e6ac5e6c0b11f23
rayallasureshkumar <rayakkasureshkumar@gmail.com>
2014-12-08 12:08:17 +0530

    paper change

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,8 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :discription, :image_content_type, :image_file_name, :image_file_size, :name
+  attr_accessible :discription, :image_content_type, :image_file_name, :image_file_size, :name, :image 
   belongs_to :album
+
+ has_attached_file :image, :styles => 
+          { :medium => "300x300>", :thumb => "100x100>" }
+          validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 end
